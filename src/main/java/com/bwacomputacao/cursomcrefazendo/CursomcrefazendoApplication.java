62879d34d1410fc4f2efc69f3f16e3f17aa571d2
bwacomputacao <bwacomputacao@gmail.com>
2022-02-22 18:58:40 -0300

    Projeto criado

---
+++ src/main/java/com/bwacomputacao/cursomcrefazendo/CursomcrefazendoApplication.java
@@ -0,0 +1,13 @@
+package com.bwacomputacao.cursomcrefazendo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcrefazendoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcrefazendoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bwacomputacao/cursomcrefazendo/CursomcrefazendoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bwacomputacao.cursomcrefazendo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcrefazendoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
