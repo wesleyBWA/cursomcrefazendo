9b5df230e2d153c6863f71f85e7971e87f2911b0
bwacomputacao <bwacomputacao@gmail.com>
2022-03-15 08:36:48 -0300

    Criando operacao de instanciacao

--- src/main/java/com/bwacomputacao/cursomcrefazendo/CursomcrefazendoApplication.java
+++ src/main/java/com/bwacomputacao/cursomcrefazendo/CursomcrefazendoApplication.java
@@ -1,13 +1,34 @@
 package com.bwacomputacao.cursomcrefazendo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcrefazendoApplication {
+import com.bwacomputacao.cursomcrefazendo.domain.Categoria;
+import com.bwacomputacao.cursomcrefazendo.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcrefazendoApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcrefazendoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+         Categoria cat1 = new Categoria(null, "Informatica");
+         Categoria cat2 = new Categoria(null, "Escritorio");
+         
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
