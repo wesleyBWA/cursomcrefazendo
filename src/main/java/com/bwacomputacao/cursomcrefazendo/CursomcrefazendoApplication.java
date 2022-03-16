ab734de37237e200046cb52d486c6333c20eb6ed
bwacomputacao <bwacomputacao@gmail.com>
2022-03-16 09:16:46 -0300

    Produtos e associacao muitos para muitos

--- src/main/java/com/bwacomputacao/cursomcrefazendo/CursomcrefazendoApplication.java
+++ src/main/java/com/bwacomputacao/cursomcrefazendo/CursomcrefazendoApplication.java
@@ -8,13 +8,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.bwacomputacao.cursomcrefazendo.domain.Categoria;
+import com.bwacomputacao.cursomcrefazendo.domain.Produto;
 import com.bwacomputacao.cursomcrefazendo.repositories.CategoriaRepository;
+import com.bwacomputacao.cursomcrefazendo.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcrefazendoApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcrefazendoApplication.class, args);
@@ -26,8 +32,19 @@ public class CursomcrefazendoApplication implements CommandLineRunner {
          Categoria cat1 = new Categoria(null, "Informatica");
          Categoria cat2 = new Categoria(null, "Escritorio");
          
-		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+         Produto p1 = new Produto(null, "computador", 2000.00);
+         Produto p2 = new Produto(null, "impresora", 800.00);
+         Produto p3 = new Produto(null, "mouse", 80.00);
+         
+         cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+         cat2.getProdutos().addAll(Arrays.asList(p2));
+         
+         p1.getCategorias().addAll(Arrays.asList(cat1));
+         p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+         p3.getCategorias().addAll(Arrays.asList(cat1));
+         
+     categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+     produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 	}
 
